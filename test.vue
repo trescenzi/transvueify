612afc985ffe4ae4c810eed5b28620395e0e196a
Thomas Crescenzi <trescenzi@gmail.com>
2017-03-06 19:28:48 -0500

    kinda ugly testing stuff

---
+++ test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>this worked</div>
+</template>
+
+<script>
+export default {
+  data: {
+    x: 42,
+  },
+  methods: {
+    poop: function() {
+      console.log('poop');
+    }
+  },
+}
+</script>
