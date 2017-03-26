760d8d137994154d38077d85e87cf9b3c37dcb03
Thomas <trescenzi@gmail.com>
2017-03-26 15:17:43 -0400

    chore(example): update example to show use of basename

---
+++ example/src/component/component.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="component">
+    <slot name="first></slot>
+    <div>{{splitter}}</div>
+    <slot name="second"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: {
+      splitter: 'hi',
+    },
+  }
+</script>
+
+<style lang="scss">
+.component {
+  &:nth-child(1) {
+    background-color: red;
+  }
+
+  &:nth-child(2) {
+    background-color: blue;
+  }
+
+  &:nth-child(3) {
+    background-color: green;
+  }
+}
+</style>
